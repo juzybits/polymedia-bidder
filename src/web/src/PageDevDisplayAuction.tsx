07b65ed9cb177d716b918374705b18f53a823d49
juzybits <96890594+juzybits@users.noreply.github.com>
2024-08-22 17:02:18 +0000

    sui: fix Display<History>

--- src/web/src/PageDevDisplayAuction.tsx
+++ src/web/src/PageDevDisplayAuction.tsx
@@ -1,63 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { svgToDataUrl, trimSvg } from "./lib/svg";
-
-const svgRaw = `
-<svg width="100%" height="100%" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
-
-  <rect width="100%" height="100%" fill="black"/>
-
-  <foreignObject width="100%" height="100%">
-    <div xmlns="http://www.w3.org/1999/xhtml"
-        style="
-            height: 100%;
-            width: 100%;
-
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            gap: 1em;
-
-            box-sizing: border-box;
-            padding: 0.66em;
-
-            font-size: 75px;
-            font-family: system-ui;
-            color: white;
-            text-align: center;
-
-            overflow-wrap: anywhere;
-        "
-    >
-
-        <div style="font-size: 1.5em">
-            <b>AUCTION</b>
-        </div>
-
-        <div>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-        </div>
-
-    </div>
-
-  </foreignObject>
-
-</svg>
-`.trim();
-
-const svgTrimmed = trimSvg(svgRaw);
-const svgDataUrl = svgToDataUrl(svgTrimmed);
-
-const displayUrl = svgDataUrl.replace(
-    "Lorem%20ipsum%20dolor%20sit%20amet%2C%20consectetur%20adipisicing%20elit%2C%20sed%20do%20eiusmod%20tempor%20incididunt%20ut%20labore",
-    "{name}",
-);
-
-console.log(displayUrl.length);
-console.log(displayUrl);
 
 export const PageDevDisplayAuction: React.FC = () =>
 {
+    const [svgDataUrl, setSvgDataUrl] = useState("");
+
+    useEffect(() =>
+    {
+        const svgRaw = `
+        <svg width="100%" height="100%" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+
+          <rect width="100%" height="100%" fill="black"/>
+
+          <foreignObject width="100%" height="100%">
+            <div xmlns="http://www.w3.org/1999/xhtml"
+                style="
+                    height: 100%;
+                    width: 100%;
+
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                    gap: 1em;
+
+                    box-sizing: border-box;
+                    padding: 0.66em;
+
+                    font-size: 75px;
+                    font-family: system-ui;
+                    color: white;
+                    text-align: center;
+
+                    overflow-wrap: anywhere;
+                "
+            >
+
+                <div style="font-size: 1.5em">
+                    <b>AUCTION</b>
+                </div>
+
+                <div>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                </div>
+
+            </div>
+
+          </foreignObject>
+
+        </svg>
+        `.trim();
+
+        const svgTrimmed = trimSvg(svgRaw);
+        const svgDataUrl = svgToDataUrl(svgTrimmed);
+        setSvgDataUrl(svgDataUrl);
+
+        const displayUrl = svgDataUrl.replace(
+            "Lorem%20ipsum%20dolor%20sit%20amet%2C%20consectetur%20adipisicing%20elit%2C%20sed%20do%20eiusmod%20tempor%20incididunt%20ut%20labore",
+            "{name}",
+        );
+
+        console.log(displayUrl.length);
+        console.log(displayUrl);
+    }, []);
+
     return (
     <div id="page-dev-display-auction" className="page-regular">
 
--- src/web/src/PageDevDisplayHistory.tsx
+++ src/web/src/PageDevDisplayHistory.tsx
@@ -1,51 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { svgToDataUrl, trimSvg } from "./lib/svg";
-
-const svgRaw = `
-<svg width="100%" height="100%" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
-
-  <rect width="100%" height="100%" fill="#002436"/>
-
-  <foreignObject width="100%" height="100%">
-    <div xmlns="http://www.w3.org/1999/xhtml"
-        style="
-            height: 100%;
-            width: 100%;
-
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            gap: 1em;
-
-            box-sizing: border-box;
-            padding: 0.66em;
-
-            font-size: 75px;
-            font-family: system-ui;
-            color: white;
-            text-align: center;
-
-            overflow-wrap: anywhere;
-        "
-    >
-
-        <div style="font-size: 1.5em">
-            <b>AUCTION HISTORY</b>
-        </div>
-
-    </div>
-
-  </foreignObject>
-
-</svg>
-`.trim();
-
-const svgTrimmed = trimSvg(svgRaw);
-const svgDataUrl = svgToDataUrl(svgTrimmed);
 
 export const PageDevDisplayHistory: React.FC = () =>
 {
+    const [svgDataUrl, setSvgDataUrl] = useState("");
+
+    useEffect(() => {
+        const svgRaw = `
+        <svg width="100%" height="100%" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+
+          <rect width="100%" height="100%" fill="#002436"/>
+
+          <foreignObject width="100%" height="100%">
+            <div xmlns="http://www.w3.org/1999/xhtml"
+                style="
+                    height: 100%;
+                    width: 100%;
+
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                    gap: 1em;
+
+                    box-sizing: border-box;
+                    padding: 0.66em;
+
+                    font-size: 100px;
+                    font-family: system-ui;
+                    color: white;
+                    text-align: center;
+
+                    overflow-wrap: anywhere;
+                "
+            >
+
+                <div style="font-size: 1.5em">
+                    <b>AUCTION HISTORY</b>
+                </div>
+
+            </div>
+
+          </foreignObject>
+
+        </svg>
+        `.trim();
+
+        const svgTrimmed = trimSvg(svgRaw);
+        const svgDataUrl = svgToDataUrl(svgTrimmed);
+        setSvgDataUrl(svgDataUrl);
+
+        console.log(svgDataUrl.length);
+        console.log(svgDataUrl);
+    }, []);
+
     return (
     <div id="page-dev-display-history" className="page-regular">
 
