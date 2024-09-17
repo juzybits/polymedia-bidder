40ebc0a40e0af745c1c390cc8acc4988edf435ac
juzybits <96890594+juzybits@users.noreply.github.com>
2024-09-17 14:32:24 +0000

    sui: User.owner + link to /user/{owner}

--- src/sdk/src/config.ts
+++ src/sdk/src/config.ts
@@ -63,8 +63,8 @@ export const AUCTION_IDS: Record<NetworkName, NetworkConfig> = {
         registryId: "",
     },
     devnet: {
-        packageId: "0xf74f57c889264b45a60d022cf731fc9a92912cb416048548e533319854252d24",
-        registryId: "0x224237594d1701496608999935549fbc43cc58bbeb3cc798cad758220e85f82e",
+        packageId: "0xa01385b2fb623ffaf416dad3f1f59f5c470adb553d57fcba189dc71ccaa713b3",
+        registryId: "0xe7a87d7f4a3325029ce06647915201e3af666e67ad7c44517f64d89b902526f2",
     },
     localnet: {
         packageId: "0x7667b4d760af8904ea5471e49a4039f8866f5ca40d66df550f5890f65d7f2b74",
