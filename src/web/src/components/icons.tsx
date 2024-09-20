38dbc8494f9fa12e5cbd464223bd3cd2728bc160
juzybits <96890594+juzybits@users.noreply.github.com>
2024-09-20 19:37:20 +0000

    web: better close modal icon

--- src/web/src/components/icons.tsx
+++ src/web/src/components/icons.tsx
@@ -68,7 +68,8 @@ export const IconCart: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
 );
 
 export const IconClose: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-    <Icon {...props} fill="black" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white">
-        <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+    <Icon {...props} fill="black" viewBox="0 0 24 24">
+        <circle className="icon-close-bg" cx="12" cy="12" r="9" strokeWidth="0" fill="black" />
+        <path className="icon-close-x" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5" />
     </Icon>
 );
