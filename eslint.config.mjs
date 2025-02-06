cebc6604845b43e5f096b5dbca2017f610a14049
juzybits <96890594+juzybits@users.noreply.github.com>
2025-02-06 21:33:17 -0100

    js: modernize

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import eslint from "@eslint/js";
 import stylistic from "@stylistic/eslint-plugin";
 import eslintPluginImport from "eslint-plugin-import";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import unusedImports from "eslint-plugin-unused-imports";
 import tsEslint from "typescript-eslint";
 
 export default [
@@ -24,13 +25,44 @@ export default [
         plugins: {
             "@stylistic": stylistic,
             import: eslintPluginImport,
+            "unused-imports": unusedImports,
         },
         rules: {
+            // === stylistic ===
             "@stylistic/jsx-quotes": [ "error", "prefer-double" ],
             "@stylistic/member-delimiter-style": [ "error", { multiline: { delimiter: "semi" }, singleline: { delimiter: "semi" } } ],
             "@stylistic/no-tabs": "error",
             "@stylistic/quotes": [ "error", "double", { avoidEscape: true } ],
             "@stylistic/semi": [ "error", "always" ],
+
+            // === imports ===
+            "import/extensions": ["error", "ignorePackages", { ts: "never", tsx: "never" }], // require .js
+            "import/first": "error",
+            "import/newline-after-import": "error",
+            "import/no-duplicates": "error",
+            "import/order": ["error", {
+                "groups": [
+                    "builtin", // Node.js built-in modules (fs, path, etc.)
+                    "external", // npm packages
+                    ["internal", "parent", "sibling", "index"] // project imports (aliased + relative)
+                ],
+                "pathGroups": [
+                    {
+                        "pattern": "@polymedia/**",
+                        "group": "external",
+                        "position": "after",
+                    }
+                ],
+                "pathGroupsExcludedImportTypes": ["builtin"],
+                "newlines-between": "always",
+                "alphabetize": {
+                    "order": "asc",
+                    "caseInsensitive": true,
+                }
+            }],
+            "unused-imports/no-unused-imports": "error",
+
+            // === typescript ===
             "@typescript-eslint/consistent-type-definitions": [ "error", "type" ],
             "@typescript-eslint/no-confusing-void-expression": "off",
             "@typescript-eslint/no-floating-promises": "off",
@@ -47,7 +79,11 @@ export default [
             "@typescript-eslint/prefer-nullish-coalescing": "off",
             "@typescript-eslint/restrict-template-expressions": "off",
             "@typescript-eslint/use-unknown-in-catch-callback-variable": "off",
+
+            // === general ===
             "no-constant-condition": "off",
+
+            // === react ===
             "react/display-name": "off",
             "react/no-unescaped-entities": "off",
             "react/prop-types": "off",
@@ -60,15 +96,9 @@ export default [
         },
     },
     {
-        files: ["src/core/**/*.ts?(x)", "src/sdk/**/*.ts?(x)"],
+        files: ["**/__tests__/**/*.ts?(x)", "src/web/src/**/*.ts?(x)", "src/react/src/**/*.ts?(x)"],
         rules: {
-            "import/extensions": ["error", "ignorePackages", { ts: "never", tsx: "never" }],
-        },
-    },
-    {
-        files: ["**/__tests__/**/*.ts?(x)"],
-        rules: {
-            "import/extensions": ["off", "ignorePackages"],
+            "import/extensions": ["off", "ignorePackages"], // don't require .js
         },
     },
 ];
