4458eb83a7f351c6fe24bd02c9d4d7e8f5f230a1
juzybits <96890594+juzybits@users.noreply.github.com>
2025-02-09 14:28:57 -0100

    web: remove @polymedia/coinmeta-react

--- src/dev/link-local.sh
+++ src/dev/link-local.sh
@@ -5,20 +5,17 @@ set -o errexit      # Exit immediately if any command returns a non-zero status
 set -o pipefail     # Prevent errors in a pipeline from being masked
 set -o xtrace       # Print each command to the terminal before execution
 
-PATH_THIS=$HOME/data/code/polymedia-bidder
-PATH_SUITCASE=$HOME/data/code/polymedia-suitcase
-PATH_COINMETA=$HOME/data/code/polymedia-coinmeta
+SCRIPT_DIR="$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )"
+PATH_PROJECT="$( cd "$SCRIPT_DIR/../.." && pwd )"
+
+PATH_SUITCASE="$PATH_PROJECT/../polymedia-suitcase"
 
 cd $PATH_SUITCASE
 pnpm build
 
-cd $PATH_COINMETA
-pnpm build
-
-cd $PATH_THIS/src/sdk
+cd $PATH_PROJECT/src/sdk
 pnpm link $PATH_SUITCASE/src/core
 
-cd $PATH_THIS/src/web
+cd $PATH_PROJECT/src/web
 pnpm link $PATH_SUITCASE/src/core
 pnpm link $PATH_SUITCASE/src/react
-pnpm link $PATH_COINMETA/src/react
